20596be50d133cbc7fbea1057cfe61b43f31ac8a
Diego <diegoramos3709@gmail.com>
2024-04-01 19:31:08 -0300

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 
@@ -15,7 +15,7 @@ function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
   const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer);
-  const { products } = useSelector((rootReducer) => rootReducer.cartReducer);
+
 
   const dispatch = useDispatch()
 
